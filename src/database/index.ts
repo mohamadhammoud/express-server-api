497a286f5d68751bad04364029a1e85fd8a0536e
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-09-11 18:39:03 +0300

    🧑‍💻 imorove initialize and added

--- src/database/index.ts
+++ src/database/index.ts
@@ -50,10 +50,26 @@ class Database {
 
   async initialize() {
     try {
-      await this.AppDataSource.initialize();
-      console.log("Data Source has been initialized!");
+      if (this.AppDataSource) {
+        await this.AppDataSource?.initialize();
+      }
+
+      if (this.AppDataSource?.isInitialized) {
+        console.log("Data Source has been initialized!");
+      }
     } catch (err) {
       console.error("Error during Data Source initialization", err);
+    }
+  }
+
+  async destroy() {
+    try {
+      if (this.AppDataSource?.isInitialized) {
+        await this.AppDataSource.destroy();
+        console.log("Data Source has been destroyed!");
+      }
+    } catch (err) {
+      console.error("Error during Data Source destroying", err);
     }
   }
 }
