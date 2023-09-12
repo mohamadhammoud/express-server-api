a9a7f56a99f5d38aad08ffe0e1969f88398ab997
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-09-12 15:10:04 +0300

    ✨ added an email, password and role to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,10 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
+export enum UserRole {
+  ADMIN = "admin",
+  INVESTOR = "investor",
+}
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn("uuid")
@@ -10,6 +15,19 @@ export class User {
 
   @Column("varchar", { length: 50 })
   lastName: string;
+
+  @Column("varchar", { unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.INVESTOR,
+  })
+  role: UserRole;
 
   @Column()
   isActive: boolean;
